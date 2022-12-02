856d33e0e6fea6d6254cfba30c5f0c4b69e7e319
Carlos Oliva <olivacarlos92@gmail.com>
2022-12-02 14:23:28 -0500

    Change custom exception message

--- src/main/java/com/tmg/automation/exceptions/ProductWasNotAddedException.java
+++ src/main/java/com/tmg/automation/exceptions/ProductWasNotAddedException.java
@@ -1,9 +1,7 @@
 package com.tmg.automation.exceptions;
 
 public class ProductWasNotAddedException extends AssertionError {
-
-    public static final String PRODUCT_NOT_ADDED_SHOPPING_CART = "The product message was added to shopping cart successfully is not visible";
-
+    public static final String PRODUCT_NOT_ADDED_SHOPPING_CART = "The product was not visible into shopping cart";
     public ProductWasNotAddedException(String message, Throwable cause) {
         super(message, cause);
     }
